20cf650ca1fd919fb9f451d0c35c6cfb5041c0f2
Riya Gautam <28554065g@student.gla.ac.uk>
2025-02-11 13:55:09 +0000

    Completed Chapter 6!

--- rango/views.py
+++ rango/views.py
@@ -1,18 +1,63 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page 
 
+def index(request): 
+    
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only-- or all if less than 5.
+    # Place the list in our context_dict dictionary (with our boldmessage!)
+    # that will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
 
-def index(request):
-    #construct a dictionary to pass to the template engine as its context. 
-    #note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict = {}
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list 
+
+    #render returns http response, applies the info given that we wanna fill in template and renders it to the user
+
+    # page stuff 
+
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = page_list
+
     return render(request, 'rango/index.html', context=context_dict)
-#render returns http response, applies the info given that we wanna fill in template and renders it to the user
+
+
+
+def show_category(request, category_name_slug):
+    #Create a context dictionary which we can pass to template rendering engine
+    context_dict = {}
+
+    try: 
+        #can we find a category name slug with given name?
+        #if we can't, the .get() method reaises a DoesNotExist exception
+        # The .get() method returns one model instance or raises and exception. 
+        category = Category.objects.get(slug=category_name_slug)
+
+        #retrieve all of the associated pages. 
+        # the filter() will return a list of page objects or an empty list
+        pages = Page.objects.filter(category=category)
+
+        #add our results list to template context under name pages
+        context_dict['pages'] = pages
+        #we can also add the category object from databse to the context dictionary
+        #We'll use this in the template to verify that category exists 
+        context_dict['category'] = category 
+    except Category.DoesNotExist: 
+        #We get here if we didn't find the specified category. Don't do anything, the template will display the no category message for us
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    # Go render the response and return it to the client 
+    return render(request, 'rango/category.html', context=context_dict)
+
+
+
+
 
 def about(request): 
     return render(request,'rango/about.html')
